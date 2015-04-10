02156e8d68697829df5db5492bd081d2c67a7263
dilihini <dineeshap@gmail.com>
2015-04-10 16:53:25 +0530

    done by dineesha
    Initial truth assignment
    Successor truth assignment

--- TT2.pl
+++ TT2.pl
@@ -15,5 +15,15 @@ variable_find(Y,Vtemp,Vout).
 variable_find(not X,Vin,Vout) :-   variable_find(X,Vin,Vout).
 
 
+/* Initial truth assignment */
+truth_initial([],[]).
+truth_initial([X|R],[0|S]) :- truth_initial(R,S).
 
+/* Successor truth assignment */
+successor(A,S) :- reverse(A,R),
+next(R,N),
+reverse(N,S).
+
+next([0|R],[1|R]).
+next([1|R],[0|S]) :- next(R,S).
 
